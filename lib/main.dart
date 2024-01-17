94fa485a05fdb10201e748a0779f734b848ea671
senateseo <dev@daveplatform.com>
2024-01-16 18:36:40 -0800

    Apply Color

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey[500],
+        backgroundColor: Colors.grey[400],
         appBar: AppBar(
           title: Text('I Am Rich!'),
           backgroundColor: Colors.grey[900],
